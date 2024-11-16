5765c1acec76c1ebc589d33066837161aaa88321
SanskarOmer <sanskaromer7@gmail.com>
2024-11-17 01:44:47 +0530

    added validation to open paricular page which is either admin or employee

---
+++ task-assigner/src/context/AuthProvider.jsx
@@ -0,0 +1,15 @@
+import React, { createContext, useEffect, useState } from 'react'
+
+export const AuthContext = createContext()
+
+const AuthProvider = ({children}) => {
+  return (
+    <div>
+      <AuthContext.Provider value={'sanskar'}>
+         {children}
+      </AuthContext.Provider>
+    </div>
+  )
+}
+
+export default AuthProvider
