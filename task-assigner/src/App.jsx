a8857184720e170428ef48cf8c1354fa5f179671
SanskarOmer <sanskaromer7@gmail.com>
2024-11-17 20:59:03 +0530

    added email and password checking to open a specific dashboard

--- task-assigner/src/App.jsx
+++ task-assigner/src/App.jsx
@@ -9,13 +9,32 @@ import { AuthContext } from './context/AuthProvider'
 const App = () => {
 
   const [user, setUser] = useState(null)
+  const [loggedInData, setLoggedInData] = useState(null)
+  const authData = useContext (AuthContext)
+
+
+// useEffect ( ( ) => {
+//     if (authData){
+//       const loggedInUser = localStorage.getItem("loggedInUser")
+//         if (loggedInUser){
+//           setUser(loggedInUser.role)
+//     }
+//   }
+// }, [authData]);
+
 
   const handleLogin = (email, password) => {
     if (email == 'admin@me.com' && password == '123') {
       setUser('admin')
+      localStorage.setItem('loggedInUser', JSON.stringify({role:'admin'}))
       
-    } else if (email == 'user@me.com' && password == '123'){
-        setUser('employee')
+    } else if (authData ){
+       const employee = authData.employees.find((e)=>email == e.email && password == e.password)
+       if (employee){
+         setUser('employee')
+         setLoggedInData(employee)
+         localStorage.setItem('loggedInUser', JSON.stringify({role:'employee'}))
+       }
       }
     
     else {
@@ -30,7 +49,7 @@ console.log(data)
   return (
     <>
       {!user ? <Login handleLogin={handleLogin} /> : ''}
-      {user == 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
+      {user == 'admin' ? <AdminDashboard /> : (user == employee ? <EmployeeDashboard data={loggedInData} /> : null) }
     </>
     
     
