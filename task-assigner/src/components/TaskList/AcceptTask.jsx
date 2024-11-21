e5bca1305213a733e3d7cd7681aa447850e2d338
SanskarOmer <sanskaromer7@gmail.com>
2024-11-21 23:39:05 +0530

    just for streak

--- task-assigner/src/components/TaskList/AcceptTask.jsx
+++ task-assigner/src/components/TaskList/AcceptTask.jsx
@@ -11,6 +11,7 @@ const AcceptTask = ({data}) => {
             <h2 className='mt-5 text-2xl font-semibold'>{data.taskTitle}</h2>
             <p className='text-sm mt-2'>
                 {data.taskDescription}
+                 
             </p>
             <div className='flex justify-between mt-6 '>
                 <button className='bg-green-500 rounded font-medium py-1 px-2 text-xs'>Mark as Completed</button>
